84002de94cde5422631cee98c168eec4ce153a19
Ted M. Young <tedyoung@gmail.com>
2021-06-21 07:40:44 -0700

    Lab #1: Characterization test for display NON-TEN ranked card.

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -15,4 +15,14 @@ class CardDisplayTest {
         assertThat(display)
                 .isEqualTo("[31m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
     }
+
+    @Test
+    public void displayNonTenAsString() throws Exception {
+        Card card = new Card(Suit.CLUBS, Rank.FIVE);
+
+        String display = card.display();
+
+        assertThat(display)
+                .isEqualTo("[30m┌─────────┐[1B[11D│5        │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│        5│[1B[11D└─────────┘");
+    }
 }
