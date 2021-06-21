297f968191dfdc2c4a6cc02fc3f6d2623bb33d0f
Ted M. Young <tedyoung@gmail.com>
2021-06-21 11:57:59 -0700

    Lab #3: Removed dead code and migrated determineOutcome to return a String instead of printing it.

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/console/ConsoleGame.java
@@ -86,7 +86,7 @@ public class ConsoleGame {
 
         displayFinalGameState(game);
 
-        game.determineOutcome();
+        System.out.println(game.determineOutcome());
 
         resetScreen();
     }
